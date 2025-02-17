cb9106a10c0b3552ba5c258408976b4e06e6daeb
Pavel.Timohodcev <43362172+Ptkuz@users.noreply.github.com>
2025-02-17 10:05:07 +0300

    JWT заработал!!!

--- CadoChat/CadoChat.Auth/CadoChat.AuthService/Program.cs
+++ CadoChat/CadoChat.Auth/CadoChat.AuthService/Program.cs
@@ -22,6 +22,7 @@ var services = builder.Services;
 
 // Генерация RSA-ключей
 var rsa = RSA.Create(2048);
+rsa.ImportRSAPrivateKey(Convert.FromBase64String("MIIEpAIBAAKCAQEAtC1E0SnF93EgjBahw/0yQ4BZpPZkeYBwp2+pHBQMBgQjgCxzxE8jhcDfOSr1KQWVUvfbWt2cVJIuLude382C2meN4TD9wKTqWhB+MgwKRJWicS2+uQS9/05LsJUR4czPLvLEMgUqhqxWiadb8slw8vaVJ3utFRNBHUJNoeOT9Ztwcy7qobq0YrbDG+DCReubYdyjbdH2ITykPZf2yNp2YX7wFG1QH0lVU4ItQoNBsTyXEyee9akvWMSiZATdWOtm4+zaMZIJGyVX6VL3EAsQ1V5QLGXyTA1N74BonBB9+NHHdy+WdoQe/JLSpGBj6syKFwQtsgrMCBxlUGkxeYgWCQIDAQABAoIBABerwti/5jRF9oKxDnuTLiFUIXLctAKKb0JwFwWLVLENpiRWsrbdtssBtdHq5N6Izz9hNL5RUxKBSfP7jalVdJWA+VDWgN/oSqmedRXaIxczmW3JFr9z8goynRsL2peRsr52QnRX3WhoB8554EibUm15G8teIjUcnHddmJlmLrAbjQC1gk0quvCk7ggFmz0r5CoccECslO+CXhZ9rFXKZ1PFbtxZ8yIMlQ3ghK+3yMf8tM0NBDxk9Hjsq1titNp6P6nyEqK1dJ1CmUfcEzducnzVHcgLNo+vZfLS9Oysv+BoWuzWIA62kZld7A+G5w3wEp+OEHAgR28khwBRk2FYKqkCgYEA7skVPHPy9W7cdwE+30P4bvPg/LYpEOAjdyqXVtm76QECjiktYaTuxiMH6RKMLUA8AVxqJYHg7ABYtA7+Qb15kyeIqRIOFq5G109BIWfoZD/eLx9B8yOgPST0uVWc7P4TDXnIDs8H6RLUGft4TJA9T/tAojDNKA1PON/gdAKGGFcCgYEAwSqH9ZO7uwvhePjtEsuBUQBANFTr/Abm8IM4rSbB3xPfE6sfHy4vxUoeu1mEiLQngu4Md1Q64yInp+xOBOLFUBKvGfe66ddG2UBqG/W/jBPggTt+i4ehw1w1tGZ0JQPG5Kac2BYh33OpCInldRsx6JeNExTBA3B8PIeW9hu9yJ8CgYEA4cGZY0tYdDT5GUZDNADmO7g1iZeLodnXjg3lgYZfw35h9Rf3QO8XlJqAGxqfDxVA5iSCcq2lglsdgjb+qhbCf58L9JUOXuEsNtpGgJflvgooPTL3PjH7iHONMEBCGkpopv/xZhbUqsZTY7E93l0sqpaoV+99t5VFxkbbxbKxJwcCgYEAmprV8wJpUU4zCsYByfdD63cN7FTEBBXqJTqB1GSe61NWSsG9yREIfxnR+xWs9FVtAmhRZfjuoPinUMnbsCFo16v8pgYXfi4lsKDTzMkmpJEMMaNSp47JNDnLajZOY4ngWQXZp0Ifnl9OPV1RYCeCDK2v5kPIMF6JsVC8zQJrJfUCgYAJe5Ag0yNKwPaCQ4Fm08s2HfzVFRSLBWKyutr9351+bpkfUyXjBRSDKjJNSTXkt7VhWJpW+ocQjP1mChk8ov0VGK8p/5gYBuy4HdY/ddjqY5icFHgZYHyrXg/072h60IyFc4CO6/7eE7u+I+xNKiy2Vt85E3efo4PM0jHhTgzyYg=="), out _);
 var signingKey = new RsaSecurityKey(rsa)
 {
     KeyId = key
@@ -41,11 +42,15 @@ services.AddIdentity<IdentityUser, IdentityRole>()
     .AddDefaultTokenProviders();
 
 // Добавляем IdentityServer
-builder.Services.AddIdentityServer()
+builder.Services.AddIdentityServer(options =>
+{
+    options.KeyManagement.Enabled = false; // ❌ Отключает автоматическую генерацию ключей
+})
     .AddSigningCredential(signingCredentials)
     .AddInMemoryClients(IdentityServerConfig.Clients)
     .AddInMemoryApiScopes(IdentityServerConfig.ApiScopes)
-    .AddInMemoryIdentityResources(IdentityServerConfig.IdentityResources);
+    .AddInMemoryIdentityResources(IdentityServerConfig.IdentityResources)
+    ;
 
 // Настройка аутентификации с использованием IdentityServer
 services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -55,24 +60,24 @@ services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.RequireHttpsMetadata = false;
         options.Audience = "chat_api";
 
+        var rsa = RSA.Create(2048);
+        rsa.ImportRSAPrivateKey(Convert.FromBase64String("MIIEpAIBAAKCAQEAtC1E0SnF93EgjBahw/0yQ4BZpPZkeYBwp2+pHBQMBgQjgCxzxE8jhcDfOSr1KQWVUvfbWt2cVJIuLude382C2meN4TD9wKTqWhB+MgwKRJWicS2+uQS9/05LsJUR4czPLvLEMgUqhqxWiadb8slw8vaVJ3utFRNBHUJNoeOT9Ztwcy7qobq0YrbDG+DCReubYdyjbdH2ITykPZf2yNp2YX7wFG1QH0lVU4ItQoNBsTyXEyee9akvWMSiZATdWOtm4+zaMZIJGyVX6VL3EAsQ1V5QLGXyTA1N74BonBB9+NHHdy+WdoQe/JLSpGBj6syKFwQtsgrMCBxlUGkxeYgWCQIDAQABAoIBABerwti/5jRF9oKxDnuTLiFUIXLctAKKb0JwFwWLVLENpiRWsrbdtssBtdHq5N6Izz9hNL5RUxKBSfP7jalVdJWA+VDWgN/oSqmedRXaIxczmW3JFr9z8goynRsL2peRsr52QnRX3WhoB8554EibUm15G8teIjUcnHddmJlmLrAbjQC1gk0quvCk7ggFmz0r5CoccECslO+CXhZ9rFXKZ1PFbtxZ8yIMlQ3ghK+3yMf8tM0NBDxk9Hjsq1titNp6P6nyEqK1dJ1CmUfcEzducnzVHcgLNo+vZfLS9Oysv+BoWuzWIA62kZld7A+G5w3wEp+OEHAgR28khwBRk2FYKqkCgYEA7skVPHPy9W7cdwE+30P4bvPg/LYpEOAjdyqXVtm76QECjiktYaTuxiMH6RKMLUA8AVxqJYHg7ABYtA7+Qb15kyeIqRIOFq5G109BIWfoZD/eLx9B8yOgPST0uVWc7P4TDXnIDs8H6RLUGft4TJA9T/tAojDNKA1PON/gdAKGGFcCgYEAwSqH9ZO7uwvhePjtEsuBUQBANFTr/Abm8IM4rSbB3xPfE6sfHy4vxUoeu1mEiLQngu4Md1Q64yInp+xOBOLFUBKvGfe66ddG2UBqG/W/jBPggTt+i4ehw1w1tGZ0JQPG5Kac2BYh33OpCInldRsx6JeNExTBA3B8PIeW9hu9yJ8CgYEA4cGZY0tYdDT5GUZDNADmO7g1iZeLodnXjg3lgYZfw35h9Rf3QO8XlJqAGxqfDxVA5iSCcq2lglsdgjb+qhbCf58L9JUOXuEsNtpGgJflvgooPTL3PjH7iHONMEBCGkpopv/xZhbUqsZTY7E93l0sqpaoV+99t5VFxkbbxbKxJwcCgYEAmprV8wJpUU4zCsYByfdD63cN7FTEBBXqJTqB1GSe61NWSsG9yREIfxnR+xWs9FVtAmhRZfjuoPinUMnbsCFo16v8pgYXfi4lsKDTzMkmpJEMMaNSp47JNDnLajZOY4ngWQXZp0Ifnl9OPV1RYCeCDK2v5kPIMF6JsVC8zQJrJfUCgYAJe5Ag0yNKwPaCQ4Fm08s2HfzVFRSLBWKyutr9351+bpkfUyXjBRSDKjJNSTXkt7VhWJpW+ocQjP1mChk8ov0VGK8p/5gYBuy4HdY/ddjqY5icFHgZYHyrXg/072h60IyFc4CO6/7eE7u+I+xNKiy2Vt85E3efo4PM0jHhTgzyYg=="), out _);
+        var signingKey = new RsaSecurityKey(rsa)
+        {
+            KeyId = builder.Configuration["Jwt:SecretKey"]
+        };
+
         options.TokenValidationParameters = new TokenValidationParameters
         {
 
+            ClockSkew = TimeSpan.Zero,
             ValidateIssuer = true,
-            ValidIssuer = "https://localhost:7220",
-            //ValidateAudience = true,
-            ValidAudience = "chat_api",
+            ValidateAudience = true,
             ValidateLifetime = true,
-            IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) =>
-            {
-                // Здесь вы можете добавить свой код для получения публичных ключей
-                // Например, запросить их у IdentityServer через jwks endpoint:
-                var jwksUri = $"{options.Authority}/.well-known/openid-configuration/jwks";
-                var client = new HttpClient();
-                var response = client.GetStringAsync(jwksUri).Result;
-                var keys = JsonConvert.DeserializeObject<JsonWebKeySet>(response);
-                return keys.Keys;
-            }
+            IssuerSigningKey = signingKey, // Здесь используется ключ для подписи
+            ValidIssuer = "https://localhost:7220", // Указание правильного издателя
+            ValidAudience = "chat_api", // Указание правильной аудитории
+            ValidateIssuerSigningKey = true // Включаем валидацию подписи
         };
 
         options.Events = new JwtBearerEvents
